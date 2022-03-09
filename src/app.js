c74986182df05555ee5e230ceb74003b738fedd5
ana carolina garcone <Ana Carolina>
2022-03-09 19:14:17 -0300

    iniciando projeto

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    res.statusCode = statusCode;
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === "production" ? "ok": error.stack
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Helloooooo!!!!!');
+});
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});

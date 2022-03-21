9ca892bca23b4e9a25f942efcacba61129d9a1a1
Ana Carolina <garcone.anaxx@gmail.com>
2022-03-21 18:28:44 -0300

    config post mongoose schema

---
+++ src/model/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Post = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    content: {
+        type: String,
+        required: true,
+        min: 1
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    timestamps: true
+}
+)
+
+module.exports = mongoose.model('Post', Post);
